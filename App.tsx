e1bb0386bbb1df0c4f1167f79075fa813ad67f49
antinucleus <antinucleuss@gmail.com>
2024-05-05 13:03:10 +0300

    feat: add @expo/vector-icons, react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -5,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Ionicons name="add-circle" size={32} color="green" />
       <StatusBar style="auto" />
     </View>
   );
